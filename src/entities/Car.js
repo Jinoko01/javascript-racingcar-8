cef301787107d90fa4651ba6c6d726e0fdebddb1
dswvgw6412 <dswvgw6412@naver.com>
2025-10-22 11:07:23 +0900

    feat(car): Implement Car object

---
+++ src/entities/Car.js
@@ -0,0 +1,34 @@
+import { Random } from '@woowacourse/mission-utils';
+
+const MIN_RANDOM_NUMBER = 0;
+const MAX_RANDOM_NUMBER = 9;
+const MOVE_FORWARD_CONDITION = 4;
+
+export default class Car {
+  #name;
+  #position;
+
+  constructor(name) {
+    this.#name = name;
+    this.#position = 0;
+  }
+
+  move() {
+    const random = Random.pickNumberInRange(
+      MIN_RANDOM_NUMBER,
+      MAX_RANDOM_NUMBER
+    );
+
+    if (random >= MOVE_FORWARD_CONDITION) {
+      this.#position += 1;
+    }
+  }
+
+  getName() {
+    return this.#name;
+  }
+
+  getPosition() {
+    return this.#position;
+  }
+}
