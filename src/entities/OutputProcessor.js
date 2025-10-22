4f776915719d4b3c0dda2be72efdc90bf0312f8c
dswvgw6412 <dswvgw6412@naver.com>
2025-10-22 14:02:06 +0900

    fix(outputProcessor): Change console message from string to constant data

--- src/entities/OutputProcessor.js
+++ src/entities/OutputProcessor.js
@@ -1,15 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
+import { SYSTEM_MESSAGE } from '../data/messages.js';
 
 export default class OutputProcessor {
   static printStatus(scores) {
     scores.forEach((score) => {
       const [carName, position] = score;
 
-      Console.print(`${carName} : ${'-'.repeat(position)}`);
+      Console.print(SYSTEM_MESSAGE.OUTPUT_CURRENT_STATUS(carName, position));
     });
+    Console.print('');
   }
 
   static printWinners(winners) {
-    Console.print(`최종 우승자 : ${winners.join(', ')}`);
+    Console.print(SYSTEM_MESSAGE.OUTPUT_WINNERS(winners));
   }
 }
