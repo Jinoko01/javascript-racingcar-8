0d096cc680fe475ebd6d07287d1d748c6523dacf
dswvgw6412 <dswvgw6412@naver.com>
2025-10-22 13:57:53 +0900

    feat(app): Add application logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import InputProcessor from './entities/InputProcessor.js';
+import Parser from './entities/Parser.js';
+import RacingController from './entities/RacingController.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inputProcessor = new InputProcessor();
+    const { carNames: carNamesInput, roundCount: roundCountInput } =
+      await inputProcessor.input();
+
+    const parser = new Parser();
+    const { carNames, roundCount } = parser.parse(
+      carNamesInput,
+      roundCountInput
+    );
+
+    const racingController = new RacingController();
+    racingController.start(carNames, roundCount);
+  }
 }
 
 export default App;
