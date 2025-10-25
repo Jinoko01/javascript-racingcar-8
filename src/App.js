fb676fe1875e37dbaf690350acaab572dfaf4381
dswvgw6412 <dswvgw6412@naver.com>
2025-10-25 17:22:19 +0900

    chore(app): Rearrange code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import InputProcessor from './entities/InputProcessor.js';
 import Parser from './entities/Parser.js';
-import CarFactory from './CarFactory.js';
+import CarFactory from './entities/CarFactory.js';
 import RacingController from './entities/RacingController.js';
 
 class App {
   async run() {
     const inputProcessor = new InputProcessor();
-    const { carNames: carNamesInput, roundCount: roundCountInput } =
-      await inputProcessor.input();
+    const { carNames: carNamesInput, roundCount: roundCountInput } = await inputProcessor.input();
 
     const parser = new Parser();
-    const { carNames, roundCount } = parser.parse(
-      carNamesInput,
-      roundCountInput
-    );
+    const { carNames, roundCount } = parser.parse(carNamesInput, roundCountInput);
 
     const carFactory = new CarFactory();
     const racingController = new RacingController(carFactory);
